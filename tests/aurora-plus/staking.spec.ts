5e93de865367865e0e7ac9d260f1dd0f9a0a17f2
Alex Mendes <alexander.h.mendes@gmail.com>
2024-05-17 00:54:17 +0100

    refactor: move wallet connect function to fixtures

--- tests/aurora-plus/staking.spec.ts
+++ tests/aurora-plus/staking.spec.ts
@@ -1,48 +1,8 @@
-import { type BrowserContext, expect, type Page } from "playwright/test"
+import { expect, type Page } from "playwright/test"
 import { AURORA_PLUS_TAG } from "../../lib/constants/tags"
 import { test } from "../../lib/fixtures"
 
-const connectWallet = async (page: Page, context: BrowserContext) => {
-  await page.evaluate(() => {
-    localStorage.setItem("ap-promo-hidden", "1")
-  })
-
-  await page.getByRole("button", { name: "Connect wallet" }).click()
-  await page
-    .getByTestId("connect-modal")
-    .getByRole("button", { name: "Connect wallet" })
-    .click()
-
-  const connectPopupPromise = context.waitForEvent("page")
-
-  await page.getByRole("button", { name: "MetaMask" }).click()
-  await page.waitForSelector("w3m-connecting-external-view")
-
-  const connectPage = await connectPopupPromise
-
-  await connectPage.waitForLoadState("domcontentloaded")
-
-  await connectPage.getByRole("button", { name: "Next" }).click()
-  await connectPage.getByRole("button", { name: "Connect" }).click()
-  await connectPage.getByRole("button", { name: "Approve" }).click()
-  await connectPage.getByRole("button", { name: "Switch network" }).click()
-
-  await page.bringToFront()
-
-  const signPopupPromise = context.waitForEvent("page")
-
-  await page
-    .getByTestId("connect-modal")
-    .getByRole("button", { name: "Accept and sign" })
-    .click()
-
-  const signPage = await signPopupPromise
-
-  await signPage.getByTestId("popover-close").click()
-  await signPage.getByRole("button", { name: "Sign" }).click()
-
-  await page.bringToFront()
-}
+test.describe.configure({ mode: "serial" })
 
 const getWalletValues = async (page: Page) => {
   // Wait for the numbers animation to finish
@@ -60,14 +20,14 @@ const getWalletValues = async (page: Page) => {
 }
 
 test.describe("Aurora Plus: Staking", { tag: AURORA_PLUS_TAG }, () => {
-  test("connects a wallet", async ({ getApp, page, metamask, context }) => {
+  test("connects a wallet", async ({ getApp, page, metamask }) => {
     await metamask.setup()
 
     const app = getApp("aurora-plus")
 
     await app.goto("/dashboard")
 
-    await connectWallet(page, context)
+    await metamask.connect()
 
     await expect(page.getByTestId("connected-indicator")).toBeVisible()
   })
@@ -79,7 +39,7 @@ test.describe("Aurora Plus: Staking", { tag: AURORA_PLUS_TAG }, () => {
 
     await app.goto("/dashboard")
 
-    await connectWallet(page, context)
+    await metamask.connect()
 
     // Wait for the numbers animation to finish
     await page.waitForTimeout(1000)
