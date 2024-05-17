471c2790260280f8690902478c39c617dfc12990
Alex Mendes <alexander.h.mendes@gmail.com>
2024-05-17 01:52:19 +0100

    feat: add unstaking test

--- tests/aurora-plus/staking.spec.ts
+++ tests/aurora-plus/staking.spec.ts
@@ -20,15 +20,6 @@ const getWalletValues = async (page: Page) => {
 }
 
 test.describe("Aurora Plus: Staking", { tag: AURORA_PLUS_TAG }, () => {
-  test("connects a wallet", async ({ auroraPlus, metamask, page }) => {
-    await metamask.setup()
-
-    await auroraPlus.goto("/dashboard")
-    await auroraPlus.connectToMetaMask()
-
-    await expect(page.getByTestId("connected-indicator")).toBeVisible()
-  })
-
   test("stakes some tokens", async ({
     metamask,
     auroraPlus,
@@ -117,6 +108,59 @@ test.describe("Aurora Plus: Staking", { tag: AURORA_PLUS_TAG }, () => {
 
     expect(newWalletValues.balance.toFixed(1)).toBe(
       (originalWalletValues.balance - 0.1).toFixed(1),
+    )
+  })
+
+  test("unstakes some tokens", async ({
+    metamask,
+    auroraPlus,
+    context,
+    page,
+  }) => {
+    await metamask.setup()
+
+    await auroraPlus.goto("/dashboard")
+    await auroraPlus.connectToMetaMask()
+
+    await page.getByTestId("withdrawal-cooldown-button").click()
+
+    const originalPendingWithdrawalAmount = Number(
+      await page.getByTestId("aurora-pending-withdrawal-amount").textContent(),
+    )
+
+    await page.getByTestId("withdrawal-cooldown-button").click()
+
+    await page.getByRole("button", { name: "Unstake", exact: true }).click()
+
+    const confirmModal = page.getByTestId("unstake-confirm-modal")
+    const confirmPopupPromise = context.waitForEvent("page")
+    const confirmModalButton = confirmModal.getByRole("button", {
+      name: "Unstake now",
+    })
+
+    await confirmModal.getByLabel("Amount").fill("0.1")
+    await confirmModalButton.click()
+
+    const confirmPopup = await confirmPopupPromise
+
+    await confirmPopup.getByRole("button", { name: "Confirm" }).click()
+
+    await page.bringToFront()
+
+    await expect(
+      confirmModalButton.getByTestId("loading-spinner"),
+    ).not.toBeVisible({ timeout: 60000 })
+
+    await page.getByTestId("withdrawal-cooldown-button").click()
+
+    const newPendingWithdrawalAmount = Number(
+      await page.getByTestId("aurora-pending-withdrawal-amount").textContent(),
+    )
+
+    await page.getByTestId("withdrawal-cooldown-button").click()
+
+    expect(newPendingWithdrawalAmount.toFixed(1)).toBe(
+      (originalPendingWithdrawalAmount + 0.1).toFixed(1),
     )
   })
 })
