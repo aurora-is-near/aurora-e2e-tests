6debc11269f7a7d7844d0a211ed9bbde7ff17f22
Gailius Besusparis <gailius.besusparis@testdevlab.com>
2024-07-16 11:26:50 +0300

    disable unnecessarty options

--- lib/fixtures/extensions.ts
+++ lib/fixtures/extensions.ts
@@ -20,21 +20,21 @@ export const extensionsTest = test.extend<{
   metamaskExtensionId: string
   baseUrl: string
 }>({
-  // eslint-disable-next-line no-empty-pattern
+  // eslint-disable-next-line no-empty-pattern, prettier/prettier
   context: async ({ }, use) => {
     const browserArgs = [
       `--disable-extensions-except=${METAMASK_EXTENSION_PATH}`,
       `--load-extension=${METAMASK_EXTENSION_PATH}`,
-      "--remote-debugging-port=9222",
+      // "--remote-debugging-port=9222",
     ]
 
     // if (process.env.CI) {
     //   browserArgs.push("--disable-gpu")
     // }
 
-    if (process.env.HEADLESS_MODE) {
-      browserArgs.push("--headless=new")
-    }
+    // if (process.env.HEADLESS_MODE) {
+    //   browserArgs.push("--headless=new")
+    // }
 
     // launch browser
     const context = await chromium.launchPersistentContext("", {
