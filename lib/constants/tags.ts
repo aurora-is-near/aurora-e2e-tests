734b43985480118a106d837fe59cc54774057f00
Gailius Besusparis <gailius.besusparis@testdevlab.com>
2024-07-31 14:48:48 +0300

    Remove unnecessary files

--- lib/constants/tags.ts
+++
@@ -1,2 +0,0 @@
-export const AURORA_PLUS_TAG = "@aurora-plus"
-export const WEB3_WALLET_TAG = "@web3-wallet"
--- lib/constants/timouts.ts
+++
@@ -1,9 +0,0 @@
-export const shortTimeout = {
-  timeout: 20000,
-}
-export const midTimeout = {
-  timeout: 60000,
-}
-export const longTimeout = {
-  timeout: 180000,
-}
