6b7ffcace60b5e9557e8fae74537b0eb319a15c9
Gailius Besusparis <gailius.besusparis@testdevlab.com>
2024-07-31 15:14:15 +0300

    add helper constants

---
+++ tests/helpers/constants/pages.ts
@@ -0,0 +1,2 @@
+export const AURORA_PLUS_PAGE = { baseURL: "https://aurora.plus" }
+export const NEAR_WEB3_PAGE = { baseURL: "https://web3-wallet-three.vercel.app" }
---
+++ tests/helpers/constants/tags.ts
@@ -0,0 +1,2 @@
+export const AURORA_PLUS_TAG = "@aurora-plus"
+export const WEB3_WALLET_TAG = "@web3-wallet"
