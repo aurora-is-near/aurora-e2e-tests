a18f9dbb14de18fd920c882f035bbbbf5e35195f
Gailius Besusparis <gailius.besusparis@gmail.com>
2024-08-27 12:18:52 +0300

    Add Web3 wallet configuration and example test

--- tests/helpers/constants/pages.ts
+++ tests/helpers/constants/pages.ts
@@ -1,2 +1,3 @@
 export const AURORA_PLUS_PAGE = { baseURL: "https://aurora.plus" }
 export const NEAR_WEB3_PAGE = { baseURL: "https://web3-wallet-three.vercel.app" }
+export const NEAR_WEB3_PAGE_TESTNET = { baseURL: "https://web3-wallet-testnet.vercel.app/" }
---
+++ tests/page_near-web3/pages/home.page.ts
@@ -0,0 +1,124 @@
+import { expect, type Locator, type Page } from "@playwright/test"
+import { BasePage } from "./base.page"
+import { setTimeout } from "timers/promises"
+
+export class HomePage extends BasePage {
+
+    swapContainer: Locator
+    swapFromWallet: Locator
+    swapToWallet: Locator
+    fromAmountInputField: Locator
+    toAmountInputField: Locator
+    firstDropdownArrow: Locator
+    secondDropdownArrow: Locator
+    amountInputField: Locator
+    swapButton: Locator
+    popUpConfirmTransactionButton: Locator
+    balance: Locator
+    successNotificationTitle: Locator
+    successNotificationCloseButton: Locator
+
+    constructor(page: Page) {
+        super(page)
+        this.page = page
+        this.swapContainer = page.getByRole("heading", { name: "Swap on NEAR" })
+        this.swapFromWallet = page
+            .locator("form")
+            .getByText("NEAR", { exact: true })
+        this.swapToWallet = page.getByText("REF", { exact: true })
+        this.fromAmountInputField = page.getByText('NEAR~$').getByPlaceholder('0.0')
+        this.toAmountInputField = page.getByText('REF~$').getByPlaceholder('0.0')
+        // Dropdown locators must be updated
+        this.firstDropdownArrow = page.locator('form').getByText('NEAR', { exact: true })
+        this.secondDropdownArrow = page.locator('form').getByText('REF', { exact: true })
+        this.amountInputField = page.getByPlaceholder('0.0')
+        this.swapButton = page.getByRole('button', { name: 'Swap' })
+        this.popUpConfirmTransactionButton = page.getByRole('button', { name: 'Confirm transaction' })
+        this.balance = page.getByText('Balance:')
+        this.successNotificationTitle = page.getByText('Success!')
+        this.successNotificationCloseButton = page.getByRole('button', { name: 'Close' })
+    }
+
+    async confirmHomePageLoaded(url: string, page = this.page) {
+        await this.confirmCorrectPageLoaded(page, url)
+    }
+
+    async scrollToSwapContainer() {
+        await this.swapContainer.scrollIntoViewIfNeeded()
+    }
+
+    async getFromTokenBalance() {
+        await setTimeout(2000)
+        const messageOnFail = '"From" token balance is not visible'
+        await expect(this.balance, messageOnFail).toBeVisible()
+        const balanceText = await this.balance.innerText()
+        return balanceText
+    }
+
+    async selectTokenToSwapFrom(tokenName: string) {
+        if (tokenName !== 'NEAR') {
+            await this.firstDropdownArrow.click();
+            // Must be updated to IDS
+            const tokenSelector = this.page.getByText(tokenName, { exact: true })
+            const messageOnFail = `Token ${tokenName} was not found in token list which contains any balance`
+            await expect(tokenSelector, messageOnFail).toBeVisible()
+            await tokenSelector.click();
+        }
+    }
+
+    async selectTokenToSwapTo(tokenName: string) {
+        if (tokenName !== 'REF') {
+            await this.secondDropdownArrow.click()
+            const tokenSelector = this.page.getByText(tokenName, { exact: true })
+            const messageOnFail = `Token ${tokenName} was not found in token list of destination tokens`
+            await expect(tokenSelector, messageOnFail).toBeVisible()
+            await tokenSelector.click();
+        }
+    }
+
+    async enterSwapFromAmount(amount: number) {
+        const messageOnFail = 'From input field is not visible'
+        await expect(this.amountInputField, messageOnFail).toBeVisible()
+        await this.amountInputField.fill(amount.toString())
+    }
+
+    async clickSwapButton() {
+        const messageOnFail = '"Swap" button is disabled, while it should be enabled'
+        await expect(this.swapButton, messageOnFail).toBeEnabled()
+        await this.swapButton.click()
+    }
+
+    async confirmTransactionPopup() {
+        const messageOnFail = '"Confirm transaction" in transaction overview pop-up not visible'
+        await expect(this.popUpConfirmTransactionButton, messageOnFail).toBeVisible()
+        await this.popUpConfirmTransactionButton.click()
+    }
+
+    async confirmSuccessNotificationAppears() {
+        const messageOnFail = 'Pop up of successfull transaction does not appear'
+        await expect(this.successNotificationTitle, messageOnFail).toBeVisible()
+        await this.successNotificationCloseButton.click()
+        await this.page.reload()
+    }
+
+    async confirmTransactionWasCorrect(balanceBeforeString: string, balanceAfterString: string, transfer: number) {
+
+        const balanceBefore = Number(balanceBeforeString.replace('Balance: ', ''))
+        const balanceAfter = Number(balanceAfterString.replace('Balance: ', ''))
+
+        expect((balanceBefore - transfer).toFixed()).toBe(balanceAfter)
+    }
+
+    async confirmSwapButtonNotAvailable() {
+        const messageOnFail = '"Swap" button is enabled, while it should be disabled'
+        await expect(this.swapButton, messageOnFail).toBeDisabled()
+    }
+
+    async restoreToDefaultTokens(tokenFrom: string, tokenTo: string) {
+        await this.page.locator('form').getByText(tokenFrom, { exact: true }).click()
+        await this.page.getByText('NEAR', { exact: true }).click()
+
+        await this.page.getByText(tokenTo, { exact: true }).click()
+        await this.page.getByText('REF', { exact: true }).click()
+    }
+}
