a9cfbd11c0b7f689a8d27902d2c93b70e23ef49a
Gailius Besusparis <gailius.besusparis@gmail.com>
2024-09-10 15:18:29 +0300

    Enable web3 wallet

--- test/page_near-web3/specs/swapping.spec.ts
+++ test/page_near-web3/specs/swapping.spec.ts
@@ -1,29 +1,29 @@
-// import { WEB3_WALLET_TAG } from "../../helpers/constants/tags"
-// import { nearEnvironment } from "../../helpers/functions/system-variables"
-// import { test } from "../fixtures/near-web3"
-// import { HomePage } from "../pages/home.page"
+import { WEB3_WALLET_TAG } from "../../helpers/constants/tags"
+import { nearEnvironment } from "../../helpers/functions/system-variables"
+import { test } from "../fixtures/near-web3"
+import { HomePage } from "../pages/home.page"
 
-// test.use(nearEnvironment("mainnet"))
+test.use(nearEnvironment("mainnet"))
 
-// test.beforeEach(
-//   "Login to Near Web3 wallet with MetaMask",
-//   async ({ nearWeb3Preconditions }) => {
-//     await nearWeb3Preconditions.loginToNearWeb3()
-//   },
-// )
+test.beforeEach(
+  "Login to Near Web3 wallet with MetaMask",
+  async ({ nearWeb3Preconditions }) => {
+    await nearWeb3Preconditions.loginToNearWeb3()
+  },
+)
 
-// test.describe("NEAR Web3 Wallet: Swapping", { tag: WEB3_WALLET_TAG }, () => {
-//   const tokenFrom = "NEAR"
+test.describe("NEAR Web3 Wallet: Swapping", { tag: WEB3_WALLET_TAG }, () => {
+  const tokenFrom = "NEAR"
 
-//   test.skip(`Confirm that user cannot swap more than the balance contains`, async ({
-//     page,
-//   }) => {
-//     const transferAmount = 9999
-//     const homePage = new HomePage(page)
-//     await homePage.confirmHomePageLoaded("/")
-//     await homePage.scrollToSwapContainer()
-//     await homePage.selectTokenToSwapFrom(tokenFrom)
-//     await homePage.enterSwapFromAmount(transferAmount)
-//     await homePage.confirmSwapButtonNotAvailable()
-//   })
-// })
+  test.skip(`Confirm that user cannot swap more than the balance contains`, async ({
+    page,
+  }) => {
+    const transferAmount = 9999
+    const homePage = new HomePage(page)
+    await homePage.confirmHomePageLoaded("/")
+    await homePage.scrollToSwapContainer()
+    await homePage.selectTokenToSwapFrom(tokenFrom)
+    await homePage.enterSwapFromAmount(transferAmount)
+    await homePage.confirmSwapButtonNotAvailable()
+  })
+})
