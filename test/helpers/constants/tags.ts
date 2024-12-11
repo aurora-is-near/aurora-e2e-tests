36dd203d495c5d01b8dad8010ff70e63ab9a6531
Gailius Besusparis <gailius.besusparis@gmail.com>
2024-12-11 15:03:37 +0200

    Add tags, add manual pipeline trigger

--- test/helpers/constants/tags.ts
+++ test/helpers/constants/tags.ts
@@ -1,2 +1,7 @@
 export const AURORA_PLUS_TAG = "@aurora-plus"
+export const AURORA_PLUS_TAG_BORROWING = "@aurora-plus-borrowing"
+export const AURORA_PLUS_TAG_DEPOSITING = "@aurora-plus-depositing"
+export const AURORA_PLUS_TAG_STAKING = "@aurora-plus-staking"
+
 export const WEB3_WALLET_TAG = "@web3-wallet"
+export const WEB3_WALLET_TAG_SAPPING = "@web3-wallet-swapping"
