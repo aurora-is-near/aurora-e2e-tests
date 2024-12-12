cbd27d252f67936d601edb008fd870bcb08ca1b4
Gailius Besusparis <gailius.besusparis@gmail.com>
2024-12-12 08:01:21 +0200

    Reduce playwright timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   forbidOnly: false,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  reporter: [["html", { outputFolder: "my-report", open: "never" }]],
-  timeout: (process.env.CI ? 180 : 60) * 60 * 1000,
+  reporter: [["line", { outputFolder: "my-report", open: "never" }]],
+  timeout: (process.env.CI ? 10 : 2) * 60 * 1000,
   use: {
     screenshot: "only-on-failure",
     trace: "on-first-retry",
@@ -17,7 +17,7 @@ export default defineConfig({
     viewport: { width: 1920, height: 1080 },
   },
   expect: {
-    timeout: (process.env.CI ? 180 : 2) * 60 * 1000,
+    timeout: (process.env.CI ? 10 : 2) * 60 * 1000,
   },
   projects: [
     {
