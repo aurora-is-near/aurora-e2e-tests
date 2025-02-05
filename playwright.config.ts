8bba0d7580d13c4782306fcb2ac4cec03d7ef8ab
roberts.bralis <roberts.bralis@testdevlab.com>
2025-02-05 16:26:50 +0200

    Remove error showing as the output call log could be really long

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
       "@estruyf/github-actions-reporter",
       <GitHubActionOptions>{
         useDetails: true,
-        showError: true,
+        showError: false,
         showTags: false,
       },
     ],
