2cf87b5b35b04dfad7f328678e27c0fa01a27424
roberts.bralis <roberts.bralis@testdevlab.com>
2025-02-05 14:58:08 +0200

    Add more configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+import type { GitHubActionOptions } from "@estruyf/github-actions-reporter"
 import { defineConfig, devices } from "@playwright/test"
 
 export default defineConfig({
@@ -9,7 +10,13 @@ export default defineConfig({
   reporter: [
     ["html", { outputFolder: "my-report", open: "never" }],
     ["junit", { outputFile: "results.xml", open: "never" }],
-    ["@estruyf/github-actions-reporter"],
+    [
+      "@estruyf/github-actions-reporter",
+      <GitHubActionOptions>{
+        useDetails: true,
+        showError: true,
+      },
+    ],
   ],
   reportSlowTests: null,
   timeout: (process.env.CI ? 3 : 2) * 60 * 1000,
