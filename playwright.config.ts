e2296829ef7f9b998469e58cc52b5b910616e5d8
roberts.bralis <roberts.bralis@testdevlab.com>
2025-03-24 14:53:34 +0200

    Fix the conditional - needed flipping

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   retries: process.env.CI ? 1 : 0,
   workers: 1,
   reporter:
-    process.env.CI && process.env.MANUAL_TRIGGER
+    process.env.CI && !process.env.MANUAL_TRIGGER
       ? [
           ["html", { outputFolder: "my-report", open: "never" }],
           ["junit", { outputFile: "results.xml", open: "never" }],
