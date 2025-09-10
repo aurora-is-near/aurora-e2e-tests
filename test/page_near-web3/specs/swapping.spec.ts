666bb7c9b6a3fc8bc2f71a8b7821dafbe736ad57
roberts.bralis <roberts.bralis@testdevlab.com>
2025-09-10 11:46:57 +0300

    Add in beforeEach assertion that Metamask account is logged in and in sync with UI

--- test/page_near-web3/specs/swapping.spec.ts
+++ test/page_near-web3/specs/swapping.spec.ts
@@ -10,68 +10,11 @@ import { NEAR_WEB3_PAGE } from "../../helpers/constants/pages"
 
 test.use(NEAR_WEB3_PAGE)
 
-export const NETWORK_PRESETS = {
-  Offline: {
-    offline: true,
-    downloadThroughput: 0,
-    uploadThroughput: 0,
-    latency: 0,
-    connectionType: "none",
-  },
-  NoThrottle: {
-    offline: false,
-    downloadThroughput: -1,
-    uploadThroughput: -1,
-    latency: 0,
-  },
-  Regular2G: {
-    offline: false,
-    downloadThroughput: (250 * 1024) / 8,
-    uploadThroughput: (50 * 1024) / 8,
-    latency: 300,
-    connectionType: "cellular2g",
-  },
-  Good2G: {
-    offline: false,
-    downloadThroughput: (450 * 1024) / 8,
-    uploadThroughput: (150 * 1024) / 8,
-    latency: 150,
-    connectionType: "cellular2g",
-  },
-  Regular3G: {
-    offline: false,
-    downloadThroughput: (750 * 1024) / 8,
-    uploadThroughput: (250 * 1024) / 8,
-    latency: 100,
-    connectionType: "cellular3g",
-  },
-  Good3G: {
-    offline: false,
-    downloadThroughput: (1.5 * 1024 * 1024) / 8,
-    uploadThroughput: (750 * 1024) / 8,
-    latency: 40,
-    connectionType: "cellular3g",
-  },
-  Regular4G: {
-    offline: false,
-    downloadThroughput: (4 * 1024 * 1024) / 8,
-    uploadThroughput: (3 * 1024 * 1024) / 8,
-    latency: 20,
-    connectionType: "cellular4g",
-  },
-  WiFi: {
-    offline: false,
-    downloadThroughput: (30 * 1024 * 1024) / 8,
-    uploadThroughput: (15 * 1024 * 1024) / 8,
-    latency: 2,
-    connectionType: "wifi",
-  },
-}
-
 test.beforeEach(
   "Login to Near Web3 wallet with MetaMask",
   async ({ nearWeb3Preconditions }) => {
     await nearWeb3Preconditions.loginToNearWeb3()
+    await nearWeb3Preconditions.confirmAccountLoggedIn()
   },
 )
 test.describe(
@@ -123,7 +66,6 @@ test.describe(
         await homePage.scrollToSwapContainer()
         await homePage.selectTokenToSwapFrom(tokenFrom)
         // wait for the account to load
-        await homePage.waitForAccountToBeLoggedIn()
         await homePage.enterSwapFromAmount(swapAmount)
         const balanceBefore = await homePage.getFromTokenBalance()
         await homePage.selectTokenToSwapTo(tokenTo)
